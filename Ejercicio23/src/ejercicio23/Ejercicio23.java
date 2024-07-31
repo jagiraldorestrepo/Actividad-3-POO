98024aef7d0568e70c2a827bfc661ae0dc351ca6
jagiraldorestrepo <134088695+jagiraldorestrepo@users.noreply.github.com>
2024-07-30 22:40:01 -0500

    ejercicio 23

---
+++ Ejercicio23/src/ejercicio23/Ejercicio23.java
@@ -0,0 +1,20 @@
+
+package ejercicio23;
+
+import java.util.Scanner;
+
+
+public class Ejercicio23 {
+
+
+    public static void main(String[] args) {
+        
+        
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.show(true);
+        
+    
+     
+    }
+    
+}
