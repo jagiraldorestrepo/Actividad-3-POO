6c843e7905e88a235c9136f453db63c7a4272c5f
jagiraldorestrepo <134088695+jagiraldorestrepo@users.noreply.github.com>
2024-07-30 22:39:09 -0500

    ejercicio 22

---
+++ Ejercicio22/src/ejercicio22/Ejercicio22.java
@@ -0,0 +1,42 @@
+
+package ejercicio22;
+import java.util.Scanner;
+
+public class Ejercicio22 {
+
+
+    public static void main(String[] args) {
+        
+        
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.show(true);
+        
+        /*
+        
+        // Crear un objeto Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+
+        // Obtener datos del usuario
+        
+        System.out.println("Ingrese nombre: ");
+        String nombre = scanner.nextLine();
+        
+        System.out.println("Ingrese salario básico por hora: ");
+        double salario = scanner.nextDouble();    
+        
+        System.out.println("Ingrese horas trabajadas: ");
+        double horas = scanner.nextDouble();    
+        
+       if (salario > 450000){ 
+           
+       System.out.println("nombre: " + nombre);
+       System.out.println("salario mensual: " + salario*horas);
+      
+       }
+       
+       else{ System.out.println("nombre: " + nombre) ;}
+           
+        */
+    }
+    
+}
