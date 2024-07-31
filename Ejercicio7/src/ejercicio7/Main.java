d1599b9bd198da221406cfcec86234691522d6cc
jagiraldorestrepo <134088695+jagiraldorestrepo@users.noreply.github.com>
2024-07-30 22:37:53 -0500

    ejercicio 7

---
+++ Ejercicio7/src/ejercicio7/Main.java
@@ -0,0 +1,35 @@
+
+package ejercicio7;
+
+import java.util.Scanner;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        double A, B;
+        
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.show(true);
+        
+        /*
+        
+        Scanner entrada = new Scanner (System.in);
+        
+        
+        System.out.println("Ingrese el valor de A: ");
+        A = entrada.nextDouble();
+        
+        System.out.println("Ingrese el valor de B: ");
+        B = entrada.nextDouble();
+        
+        if (A > B){ System.out.println("A es mayor que B: ");}
+        
+        else if (A == B){ System.out.println("A es igual que B: "); }
+        
+        else { System.out.println("A es menor que B: "); }
+        */
+    }
+    
+}
